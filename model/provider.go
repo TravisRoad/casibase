b7f47357019f292d02bc145e04784aa51c42640c
Travis <lxymontage@gmail.com>
2024-02-29 23:37:32 +0800

    update ernie

--- model/provider.go
+++ model/provider.go
@@ -56,7 +56,7 @@ func GetModelProvider(typ string, subType string, clientId string, clientSecret 
 	} else if typ == "OpenRouter" {
 		p, err = NewOpenRouterModelProvider(subType, clientSecret, temperature, topP)
 	} else if typ == "Ernie" {
-		// p, err = NewErnieModelProvider(subType, clientId, clientSecret, temperature, topP, presencePenalty)
+		p, err = NewErnieModelProvider(subType, clientId, clientSecret, temperature, topP, presencePenalty)
 	} else if typ == "iFlytek" {
 		// p, err = NewiFlytekModelProvider(subType, clientSecret, temperature, topK)
 	} else if typ == "ChatGLM" {
